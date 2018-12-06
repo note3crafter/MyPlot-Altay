014af6400978b52fc7368ecbdab00d590c67e201
jasonwynn10 <jasonwynn10@gmail.com>
2018-12-06 15:23:11 -0500

    Make homes of other users public This feature was requested by a few individuals on the discord but will not be added into an official MyPlot release

--- src/MyPlot/subcommand/HomeSubCommand.php
+++ src/MyPlot/subcommand/HomeSubCommand.php
@@ -26,14 +26,22 @@ class HomeSubCommand extends SubCommand
 	 */
 	public function execute(CommandSender $sender, array $args) : bool {
 		if(empty($args)) {
+			$selected = $sender;
+		    $selectedName = $sender->getName();
 			$plotNumber = 1;
-		}elseif(is_numeric($args[0])) {
-			$plotNumber = (int) $args[0];
+		}elseif(isset($args[0]) and ($selected = $this->getPlugin()->getServer()->getPlayer($args[0])) !== false) {
+			$selectedName = $selected->getName();
+			if(stripos($selectedName, "me") !== false) {
+				$selectedName = $sender->getName();
+			}
+			if(!isset($args[1]) or !is_numeric($args[1]))
+				return false;
+			$plotNumber = (int) $args[1];
 		}else{
 			return false;
 		}
-		$levelName = $args[1] ?? $sender->getLevel()->getFolderName();
-		$plots = $this->getPlugin()->getPlotsOfPlayer($sender->getName(), $levelName);
+		$levelName = $args[2] ?? $sender->getLevel()->getFolderName();
+		$plots = $this->getPlugin()->getPlotsOfPlayer($selectedName, $levelName);
 		if(empty($plots)) {
 			$sender->sendMessage(TextFormat::RED . $this->translateString("home.noplots"));
 			return true;
@@ -49,7 +57,7 @@ class HomeSubCommand extends SubCommand
 			return ($plot1->levelName < $plot2->levelName) ? -1 : 1;
 		});
 		$plot = $plots[$plotNumber - 1];
-		if($this->getPlugin()->teleportPlayerToPlot($sender, $plot)) {
+		if($this->getPlugin()->teleportPlayerToPlot($selected, $plot)) {
 			$sender->sendMessage($this->translateString("home.success", [$plot, $plot->levelName]));
 		}else{
 			$sender->sendMessage(TextFormat::RED . $this->translateString("home.error"));
