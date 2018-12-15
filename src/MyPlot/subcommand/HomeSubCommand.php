26c03ba7016fbe72a3566040dbcb636df6485163
jasonwynn10 <jasonwynn10@me.com>
2018-12-15 14:50:12 -0500

    Fixed command sender not being teleported

--- src/MyPlot/subcommand/HomeSubCommand.php
+++ src/MyPlot/subcommand/HomeSubCommand.php
@@ -25,22 +25,20 @@ class HomeSubCommand extends SubCommand
 	 * @return bool
 	 */
 	public function execute(CommandSender $sender, array $args) : bool {
+		$selected = null;
 		if(empty($args)) {
 			$selected = $sender;
 		    $selectedName = $sender->getName();
 			$plotNumber = 1;
 		}elseif(isset($args[0]) and ($selected = $this->getPlugin()->getServer()->getPlayer($args[0])) !== null) {
 			$selectedName = $selected->getName();
-			if(stripos($selectedName, "me") !== false) {
-				$selectedName = $sender->getName();
-			}
 			if(!isset($args[1]) or !is_numeric($args[1]))
 				return false;
 			$plotNumber = (int) $args[1];
 		}else{
 			return false;
 		}
-		$levelName = $args[2] ?? $sender->getLevel()->getFolderName();
+		$levelName = $args[2] ?? $selected->getLevel()->getFolderName();
 		$plots = $this->getPlugin()->getPlotsOfPlayer($selectedName, $levelName);
 		if(empty($plots)) {
 			$sender->sendMessage(TextFormat::RED . $this->translateString("home.noplots"));
@@ -57,7 +55,7 @@ class HomeSubCommand extends SubCommand
 			return ($plot1->levelName < $plot2->levelName) ? -1 : 1;
 		});
 		$plot = $plots[$plotNumber - 1];
-		if($this->getPlugin()->teleportPlayerToPlot($selected, $plot)) {
+		if($this->getPlugin()->teleportPlayerToPlot($sender, $plot)) {
 			$sender->sendMessage($this->translateString("home.success", [$plot, $plot->levelName]));
 		}else{
 			$sender->sendMessage(TextFormat::RED . $this->translateString("home.error"));
